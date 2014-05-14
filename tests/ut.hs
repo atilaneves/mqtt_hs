0b677f7183486f01062b3f5a5c4b920507baeb10
Atila Neves <aalvesne@cisco.com>
2014-05-14 16:18:28 +0200

    Cabal test with no tests

--- tests/ut.hs
+++ tests/ut.hs
@@ -1,16 +1,7 @@
+module Main where
+
+import Test.Framework (defaultMain)
 import Test.HUnit
 
-test1 = TestCase (assertEqual "for (foo 3)," (1,2) (foo 3))
-test2 = TestCase (do (x,y) <- partA 3
-                     assertEqual "for the first result of partA," 5 x
-                     b <- partB y
-                     assertBool ("(partB " ++ show y ++ ") failed") b)
-
-
-tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
-foo = 4
-partA = 3
-partB = 4
-
 main :: IO ()
-main = putStrLn "UT main"
+main = defaultMain []
