4840f2c54c0bb5ace56fe3bef8b7b2d890884ad2
Atila Neves <aalvesne@cisco.com>
2014-09-07 19:23:37 +0200

    Actually passing unit test for connack

--- src/Mqtt/Broker.hs
+++ src/Mqtt/Broker.hs
@@ -1,6 +1,7 @@
-module Mqtt.Broker (getReplies, Reply) where
+module Mqtt.Broker (getReplies, getMessageType, Reply) where
 
-import Data.ByteString.Lazy (uncons, pack, unpack)
+import Data.ByteString.Lazy (uncons, pack)
+import Data.Bits (shiftR)
 import qualified Data.ByteString.Lazy as BS
 
 type Topic = String
@@ -9,5 +10,11 @@ type Reply a = (a, BS.ByteString) -- a is a handle type
 
 getReplies :: a -> BS.ByteString -> [Subscription] -> [Reply a]
 getReplies _ (uncons -> Nothing) _ = []
-getReplies handle (uncons -> Just (connect, xs)) subscriptions = [(handle, pack [32, 2, 0, 0])]
-                                                                 where connect = 1
+getReplies handle packet _ -- 1st _ is xs, 2nd subscriptions
+    | getMessageType packet == 1 = [(handle, pack [32, 2, 0, 0])]
+    | otherwise = []
+
+
+getMessageType :: BS.ByteString -> Int
+getMessageType (uncons -> Nothing) = 0
+getMessageType (uncons -> Just (connect, _)) = fromIntegral $ connect `shiftR` 4
--- tests/Mqtt/Broker/Test.hs
+++ tests/Mqtt/Broker/Test.hs
@@ -3,30 +3,39 @@ module Mqtt.Broker.Test (testConnack) where
 import Test.Framework (testGroup)
 import Test.Framework.Providers.HUnit
 import Test.HUnit
-import Data.ByteString.Lazy (uncons, pack, unpack)
+import Data.ByteString.Lazy (pack)
 import Data.Char (ord)
 import Data.Word (Word8)
-import Mqtt.Broker (getReplies, Reply)
+import Mqtt.Broker (getReplies, getMessageType, Reply)
 
-testConnack = testGroup "Encoding" [ testCase "Test MQTT reply includes CONNACK" test_decode_mqtt_connect]
+testConnack = testGroup "Encoding" [ testCase "Test MQTT reply includes CONNACK" test_decode_mqtt_connect,
+                                     testCase "Test getting message type" test_get_message_type]
 
 
+-- Helper to transform a character into a byte
 char :: Char -> Word8
 char x = (fromIntegral $ ord x) :: Word8
 
 
+-- Test that message types are returned correctly from a byte string
+test_get_message_type :: Assertion
+test_get_message_type = do
+  getMessageType (pack [0x10, 0x2a]) @?= 1
+  getMessageType (pack [0x20, 0x2a]) @?= 2
 
+-- Test that we get a MQTT CONNACK in reply to a CONNECT message
 test_decode_mqtt_connect :: Assertion
 test_decode_mqtt_connect = getReplies 3 request [] @?= ([(3, pack [32, 2, 0, 0])] :: [Reply Integer])
                            where request = pack $ [0x10, 0x2a, -- fixed header
-                                                        0x00, 0x06] ++
-                                          [char 'M', char 'Q', char 'I', char 's', char 'd', char 'p']++
+                                                   0x00, 0x06] ++
+                                          [char 'M', char 'Q', char 'I', char 's', char 'd', char 'p'] ++
                                           [0x03, -- protocol version
                                            0xcc, -- connection flags 1100111x user, pw, !wr, w(01), w, !c, x
                                            0x00, 0x0a, -- keepalive of 10
                                            0x00, 0x03, char 'c', char 'i', char 'd', -- client ID
                                            0x00, 0x04, char 'w', char 'i', char 'l', char 'l', -- will topic
                                            0x00, 0x04, char 'w', char 'm', char 's', char 'g', -- will msg
-                                           0x00, 0x07, char 'g', char 'l', char 'i', char 'f', char 't', char 'e', char 'l', --username
-                                                   0x00, 0x02, char 'p', char 'w'] --password
+                                           0x00, 0x07,
+                                           char 'g', char 'l', char 'i', char 'f', char 't', char 'e', char 'l', --username
+                                           0x00, 0x02, char 'p', char 'w'] --password
                                 -- replies = Mqtt.Broker.getReplies request
