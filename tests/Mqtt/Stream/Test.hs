28edfff3ab76060fdb4217bdd07a304a8674cf44
Atila Neves <aalvesne@cisco.com>
2014-10-12 16:14:09 +0200

    Corrected the name of the message in the stream tests

--- tests/Mqtt/Stream/Test.hs
+++ tests/Mqtt/Stream/Test.hs
@@ -26,14 +26,14 @@ char :: Char -> Word8
 char x = (fromIntegral $ ord x) :: Word8
 
 
-connectMsg :: BS.ByteString
-connectMsg = pack $ [0x8c, 0x10, -- fixed header
-                     0x00, 0x21, -- message ID
-                     0x00, 0x05, char 'f', char 'i', char 'r', char 's', char 't',
-                     0x01, -- qos
-                     0x00, 0x03, char 'f', char 'o', char 'o',
-                     0x02 -- qos
-                    ]
+subscribeMsg :: BS.ByteString
+subscribeMsg = pack $ [0x8c, 0x10, -- fixed header
+                       0x00, 0x21, -- message ID
+                       0x00, 0x05, char 'f', char 'i', char 'r', char 's', char 't',
+                       0x01, -- qos
+                       0x00, 0x03, char 'f', char 'o', char 'o',
+                       0x02 -- qos
+                      ]
 
 
 testNoBytes :: Assertion
@@ -51,9 +51,9 @@ testOnlyConnectHeader = nextMessage connectHdr @?= (emptyByteStr, connectHdr)
 
 
 testOnlyConnectMsg :: Assertion
-testOnlyConnectMsg = nextMessage connectMsg @?= (connectMsg, emptyByteStr)
+testOnlyConnectMsg = nextMessage subscribeMsg @?= (subscribeMsg, emptyByteStr)
 
 
 testConnectMsgThenGarbage :: Assertion
-testConnectMsgThenGarbage = nextMessage (connectMsg `append` garbage) @?= (connectMsg, garbage)
+testConnectMsgThenGarbage = nextMessage (subscribeMsg `append` garbage) @?= (subscribeMsg, garbage)
     where garbage = pack [1, 2, 3]
